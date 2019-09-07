0dd7eb11a0fea6bd00dce2bdc3bc2659e09f7a17
Ephraim Aigbefo <aigbefoephraim@yahoo.com>
2019-09-07 07:06:49 +0100

    Updates

---
+++ precache-manifest.91d91d184422cd007a4b21a43f9a6ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a23614e68b954715dfa0c95cf039e1",
+    "url": "/car-pooling-ng/index.html"
+  },
+  {
+    "revision": "81c1be16c72a9ab3f81c",
+    "url": "/car-pooling-ng/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb5faf65440830d59044",
+    "url": "/car-pooling-ng/static/js/2.74a018ca.chunk.js"
+  },
+  {
+    "revision": "81c1be16c72a9ab3f81c",
+    "url": "/car-pooling-ng/static/js/main.144ef35a.chunk.js"
+  },
+  {
+    "revision": "ec36c54e29484a617dea",
+    "url": "/car-pooling-ng/static/js/runtime~main.fbb3a634.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/car-pooling-ng/static/media/logo.5d5d9eef.svg"
+  }
+]);
